6adc3472fc8fe58ea30776f2f81effbf8d868b79
Dhruv Mishra <57394467+dhruvmishra144@users.noreply.github.com>
2025-03-26 14:38:59 +0530

    added firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -664,7 +664,7 @@ export default function Home() {
                 <p className='text-zinc-400 text-md'>{items.metricDetailTwo}</p>
                 </div>
               </div>
-              <Image width={'400'} height={'400'} className='h-full w-full rounded-xl' src={items.img} alt="" />
+              <Image height={'400'} className='h-full w-full rounded-xl' src={items.img} alt="" />
             </a>)
           })
         }
