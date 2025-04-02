65889d5a524e7a15465894455168d0c21955f248
Dhruv Mishra <57394467+dhruvmishra144@users.noreply.github.com>
2025-04-02 16:17:03 +0530

    fixed code

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,12 +4,12 @@ import { usePathname } from "next/navigation";
 const Footer = () => {
     const pathname = usePathname();
     return (
-        <div className="container mx-auto flex flex-col items-center text-center px-4 pb-32">
+        <div className={`container mx-auto flex flex-col items-center text-center px-4 pb-32 ${pathname==="/contact"?"hidden":""}`}>
             <Image className="mb-4" width={64} height={64} src={"/chat.svg"} alt={"lets's Connect"}  />
             <h3 className="text-3xl font-semibold mb-4">Have an idea? I&apos;d love to hear it!</h3>
             <p  className="text-base mb-4">Excited for a design journey or seeking product design tips? Reach out for some creative collaboration!</p>
             <div className="relative">
-            <a className="py-4 px-8 bg-zinc-800 hover:bg-zinc-700 rounded transition-all duration-500 btnn block" href={pathname==="/contact"?"#":"/contact"}>Let&apos;s Connect</a>
+            <a className={`py-4 px-8 bg-zinc-800 hover:bg-zinc-700 rounded transition-all duration-500 btnn block1`} href={pathname==="/contact"?"#":"/contact"}>Let&apos;s Connect</a>
             </div>
         </div>
     );
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,36 +8,38 @@ export default function About() {
   gsap.registerPlugin(ScrollTrigger);
   const intro = useRef(null);
   // Using useGSAP to run animation on mount
-  useGSAP(() => {
-    const mm = gsap.matchMedia()
+  // useGSAP(() => {
+  //   const mm = gsap.matchMedia()
    
-    mm.add("(min-width: 992px) and (prefers-reduced-motion: no-preference)", () => {
-      // desktop setup code here...
+  //   mm.add("(min-width: 992px) and (prefers-reduced-motion: no-preference)", () => {
+  //     // desktop setup code here...
     
 
-    });
+  //   });
 
-    mm.add("(max-width: 991px) and (min-width: 578px) and (prefers-reduced-motion: no-preference)", () => {
-      // tab setup code here...
+  //   mm.add("(max-width: 991px) and (min-width: 578px) and (prefers-reduced-motion: no-preference)", () => {
+  //     // tab setup code here...
       
 
-    });
+  //   });
 
-    mm.add("(max-width: 575px) and (prefers-reduced-motion: no-preference)", () => {
-      // tab setup code here...
+  //   mm.add("(max-width: 575px) and (prefers-reduced-motion: no-preference)", () => {
+  //     // tab setup code here...
 
-    });
+  //   });
 
-  }, []);
+  // }, []);
   return (
     <main ref={intro} className='container mx-auto'>
       <div className='flex flex-col items-center justify-center h-[calc(100vh-150px)] md:h-[calc(100vh-150px)]'>
       <p  className='relative text-5xl text-center text-zinc-600 mb-4'>
       I&apos;m happy to know that
       </p>
-      <p className='relative text-5xl text-center text-zinc-200'>
-      you&apos;re interested in learning more about me!
+      <p className='relative text-5xl text-center text-zinc-200 mb-8'>
+      you&apos;re interested in learning more about me
       </p>
+      <p className='relative text-left text-zinc-200 mb-6 text-2xl max-w-[1024]'>For me, it's about more than just aesthetics. I'm driven to create products that make a meaningful difference.</p>
+      <p className='relative text-left text-zinc-200 mb-6 text-2xl max-w-[1024]'>Understanding diverse perspectives cultivates empathy towards our users and strengthens my connection with the team.</p>
       </div>
      
       
