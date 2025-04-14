a08eb74d87bc760344dc02e3fdb06c26d7a4c948
Dhruv Mishra <57394467+dhruvmishra144@users.noreply.github.com>
2025-04-14 16:00:26 +0530

    fixed footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,14 +1,30 @@
 'use client'
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { animationPageOut } from "../utils/animation";
 const Footer = () => {
-    const pathname = usePathname();
+
+    const router = useRouter();
+    const href = '/contact'
+        const pathname = usePathname();
+        const [isloading, setIsLoading] = useState(false);
+        const handleCLick = () => {
+            setIsLoading(true);
+            if (pathname != href) {
+                setTimeout(() => {
+                    setIsLoading(false);
+                  }, 1500); // Transition duration: 3 seconds
+                  animationPageOut(href, router)    
+            }
+        }
+
     return (
         <div className={`container mx-auto flex flex-col items-center text-center px-4 pb-32 ${pathname==="/contact"?"hidden":""}`}>
             <Image className="mb-4" width={64} height={64} src={"/chat.svg"} alt={"lets's Connect"}  />
             <h3 className="text-3xl font-semibold mb-4">Have an idea? I&apos;d love to hear it!</h3>
             <p  className="text-base mb-4">Excited for a design journey or seeking product design tips? Reach out for some creative collaboration!</p>
-            <a className={`py-4 px-8 bg-cyan-800 hover:bg-cyan-700 rounded transition-all duration-500 btnn block1`} href={pathname==="/contact"?"#":"/contact"}>Let&apos;s Connect</a>
+            <button className={`py-4 px-8 bg-cyan-800 hover:bg-cyan-700 rounded transition-all duration-500 btnn block1`} onClick={handleCLick}>Let&apos;s Connect</button>
         </div>
     );
 }
