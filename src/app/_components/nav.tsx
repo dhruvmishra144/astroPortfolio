e4b70d47ef7b2fa6e2849c83246e2985e1b6e828
Dhruv Mishra <57394467+dhruvmishra144@users.noreply.github.com>
2025-04-15 09:51:53 +0530

    reverted nav code

--- src/app/_components/nav.tsx
+++ src/app/_components/nav.tsx
@@ -3,23 +3,23 @@ import TransitionLink from './transition-link';
 // icons
 import { HiHome, HiUser, HiEnvelope, HiMiniDocumentText } from 'react-icons/hi2';
 
+const navLinks = [
+  { href: '/', label: 'home', icon: <HiHome /> },
+  { href: '/about', label: 'about', icon: <HiUser /> },
+  { href: '/contact', label: 'contact', icon: <HiEnvelope /> },
+  { href: '/resume', label: 'resume', icon: <HiMiniDocumentText /> },
+];
+
 const Nav = () => {
-    return (
-        <div className='flex flex-wrap gap-4 md:gap-2 justify-center fixed bottom-0 sm:bottom-4 left-[50%] -translate-x-[50%] w-[calc(100%)] sm:w-auto backdrop-blur-sm bg-cyan-950/50 hover:bg-cyan-900/50 transition-all duration-300 rounded-lg z-20 px-1 py-2 sm:p-2 cursor-pointer'>
-            <TransitionLink href="/" label="home">
-                <HiHome />
-            </TransitionLink>
-            <TransitionLink href="/about" label="about">
-                <HiUser />
-            </TransitionLink>
-            <TransitionLink href="/contact" label="contact">
-                <HiEnvelope />
-            </TransitionLink>
-            <TransitionLink href="/resume" label="resume">
-                <HiMiniDocumentText />
-            </TransitionLink>
-        </div>
-    );
+  return (
+    <div className='flex flex-wrap gap-4 md:gap-2 justify-center fixed bottom-0 sm:bottom-4 left-[50%] -translate-x-[50%] w-[calc(100%)] sm:w-auto backdrop-blur-sm bg-cyan-950/50 hover:bg-cyan-900/50 transition-all duration-300 rounded-lg z-20 px-1 py-2 sm:p-2 cursor-pointer'>
+      {navLinks.map(({ href, label, icon }) => (
+        <TransitionLink key={href} href={href} label={label}>
+          {icon}
+        </TransitionLink>
+      ))}
+    </div>
+  );
 };
 
 export default Nav;
