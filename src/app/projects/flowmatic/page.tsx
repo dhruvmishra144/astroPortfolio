ad997e3d316ccd0941329e462f93620b989bc04b
Dhruv Mishra <57394467+dhruvmishra144@users.noreply.github.com>
2025-05-02 10:16:31 +0530

    fixed heading

--- src/app/projects/flowmatic/page.tsx
+++ src/app/projects/flowmatic/page.tsx
@@ -1,12 +1,13 @@
-const flowmatic = () => {
+import Image from "next/image";
 
-    const buttonList = ['Home', 'About', 'Services', 'Contact', 'Blog', 'Careers','Home', 'About', 'Services', 'Contact', 'Blog', 'Careers','Home', 'About', 'Services', 'Contact', 'Blog', 'Careers',];
+const flowmatic = () => {
     return (
         <div className="container mx-auto px-4 py-20">
             <main className="px-24">
                 <p className="text-2xl font-medium mb-4">FlowMatic</p>
-                <h1 className="text-2xl lg:text-6xl font-extralight leading-tight mb-6">Next-Gen AI iPaaS: Streamlined Workflows, Rapid Integrations</h1>
-                
+                <h1 className="text-2xl lg:text-6xl font-extralight leading-tight">Next-Gen AI iPaaS: Streamlined Workflows, Rapid Integrations</h1>
+                <p className=""></p>
+                <Image src={""} alt={""}/>
             </main>
 
         </div>
