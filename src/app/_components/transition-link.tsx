37b6e33edb2c7a6423f32d3dfcc9e749eae7cc7a
dhruvmishra144 <dhruvmishra144@gmail.com>
2026-01-22 08:01:38 +0000

    fixed wrapping issue

--- src/app/_components/transition-link.tsx
+++ src/app/_components/transition-link.tsx
@@ -16,7 +16,7 @@ const TransitionLink = ({ href, label, children }: Props) => {
     return (
         <button onClick={handleCLick} className={`rounded-lg flex flex-col gap-3 items-center uppercase p-0 py-2 sm:px-2 text-[10px] w-[60px] sm:w-[70px] cursor-pointer border border-cyan-300/0 hover:border hover:border-cyan-800 transition-all duration-300`}>
             <span className="text-lg">{children}</span>
-            <span className="text-xs">{label}</span>
+            <span className="text-xs whitespace-nowrap">{label}</span>
         </button>
     )
 }
